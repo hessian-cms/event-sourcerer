4142c93b9143d059763727a1a4a9ff6af159d02a
no4any <martin@karry.de>
2026-05-17 21:52:51 +0200

    Added linter

--- packages/lib/event-sourcerer/src/EventSourcerer.class.ts
+++ packages/lib/event-sourcerer/src/EventSourcerer.class.ts
@@ -1,5 +1,5 @@
 import { Mutex } from '@event-sourcerer/mutex'
-import { Event, EventType, EventTypeStore, EventTypeStoreInMemory } from './types';
+import { EventType, EventTypeStore, EventTypeStoreInMemory } from './types';
 
 /**
  * The main class of the library, responsible for managing events and their listeners.
@@ -9,7 +9,7 @@ export class EventSourcerer {
         const mutex = new Mutex();
         this.addEvent = mutex.lock(this.addEvent.bind(this));
         this.removeEvent = mutex.lock(this.removeEvent.bind(this));
-        this.event = mutex.lock(this.event.bind(this));
+        //this.event = mutex.lock(this.event.bind(this));
     }
 
     async addEvent(name: string, configuration: EventType): Promise<void> {
@@ -20,6 +20,7 @@ export class EventSourcerer {
         await this.eventConfigurationStore.removeEventType(name);
     }
 
+    /*
     async addEventListener<T>(name: string, listener: (event: Event<T>, configuration: EventType) => Promise<void>) {
         // TODO: implement
         throw new Error("Not implemented");
@@ -29,9 +30,10 @@ export class EventSourcerer {
         // TODO: implement
         throw new Error("Not implemented");
     }
-
+    
     async event<T>(event: Event<T>) {
         // TODO: implement
         throw new Error("Not implemented");
     }
+    */
 }
---
+++ packages/lib/mutex/eslint.config.mjs
@@ -0,0 +1,3 @@
+import eslintConfig from "@event-sourcerer/lint-config"
+
+export default eslintConfig;
