84635b98f0505dad6df28c144713099d3a138aac
no4any <martin@karry.de>
2026-05-18 19:12:04 +0200

    #1 fixxed import

--- packages/lib/event-sourcerer/src/types/implementation/EventChainInMemory.class.ts
+++ packages/lib/event-sourcerer/src/types/implementation/EventChainInMemory.class.ts
@@ -5,13 +5,13 @@ import { EventChain } from "../EventChain.abstract";
  * The EventChainInMemory class is a concrete implementation of the EventChain abstract class that uses an in-memory array to store events. It provides methods for appending events to the chain and retrieving all events from the chain. This implementation is suitable for scenarios where persistence is not required and the event chain can be stored in memory.
  */
 export class EventChainInMemory<T> extends EventChain<T> {
-    private chain: Event<T>[] = [];
+    private chain: EventData<T>[] = [];
 
-    async append(obj: Event<T>): Promise<void> {
+    async append(obj: EventData<T>): Promise<void> {
         this.chain.push(obj);
     }
 
-    async getObjects(): Promise<Event<T>[]> {
+    async getObjects(): Promise<EventData<T>[]> {
         return this.chain;
     }
 }
